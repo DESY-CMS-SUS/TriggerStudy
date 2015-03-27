df3471e58b1c2a9707018fb5f5a566c23d08ab4b
Artur Lobanov <artur.lobanov@desy.de>
2015-03-27 16:22:49 +0100

    Fix submit script

--- SimpleHLTAnalyzer/test/Batch/SubmitHLT.sh
+++ SimpleHLTAnalyzer/test/Batch/SubmitHLT.sh
@@ -5,5 +5,4 @@ Nevents=500
 OutDir=Output/TT_nojets
 #OutDir=test
 
-#qsub -t 1-$Njobs -o logs  HLTtupleJob.sh $Nevents $OutDir
-qsub -t 2001-6000 -o logs  HLTtupleJob.sh $Nevents $OutDir
+qsub -t 1-$Njobs -o logs  HLTtupleJob.sh $Nevents $OutDir
